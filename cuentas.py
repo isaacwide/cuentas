ea91f53181616fc757781c7ff8a677d9a9f2a1ac
isaacwide <isaac.aguilard@alumno.buap.mx>
2026-01-20 20:54:24 -0600

    mas complejidad

--- cuentas.py
+++ cuentas.py
@@ -1,24 +1,31 @@
 import random 
 
 def muliplicacion(estado):
-  
-    if estado is None :
+    
+    if estado == 0 :
         a = random.randint(1,10)
         b = random.randint(1,10)
-
+    if estado == 1 :
+        a = random.randint(10,20)
+        b = random.randint(10,20)
+    if estado == 2 :
+        a = random.randint(21,40)
+        b = random.randint(21,40)
+    if estado == 3 :
+       print("VAMOS A ALOCARNOS ")
+       a = random.randint(21,40)
+       b = random.randint(21,40)
+       
     print(f"{a} x {b}")
     respuesta = int(input("Respuesta......"))
 
     if (a*b) == respuesta:
        return True
-       
-
-
-
+    return False
 
 def _main() -> None:
   live = 3
-  estado = None
+  estado = 0 
   pinter = False 
   alive = True
   contador = 0 
@@ -38,8 +45,9 @@ def _main() -> None:
        print("DEAD")
 
     if contador == 3 :
+       contador = 0 
+       estado = estado +1  
        print("GANADOR")
-       break
     
 
 if __name__ == '__main__':
